f1abfcc8f62a35bc768f34a2971ec13b8ca8fc21
HyeongUn Kim <guddns@gmail.com>
2014-01-06 12:46:42 +0900

    no message

---
+++ JSONKit/JSONKit.h
@@ -0,0 +1,13 @@
+//
+//  JSONKit.h
+//  JSONKit
+//
+//  Created by hug on 2014. 1. 6..
+//  Copyright (c) 2014년 hug. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSONKit : NSObject
+
+@end
---
+++ JSONKit/JSONKit.m
@@ -0,0 +1,13 @@
+//
+//  JSONKit.m
+//  JSONKit
+//
+//  Created by hug on 2014. 1. 6..
+//  Copyright (c) 2014년 hug. All rights reserved.
+//
+
+#import "JSONKit.h"
+
+@implementation JSONKit
+
+@end
